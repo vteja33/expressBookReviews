cee058376d4ab20099df96d45070d3f9d83d7dac
Vamshi Teja Vallala <vteja33@gmail.com>
2025-03-21 21:40:31 -0400

    final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,54 +25,83 @@ public_users.post("/register", (req,res) => {
   }
 });
 
+function getBooks() {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    })
+}
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  const prettyBooks = JSON.stringify(books, null, 4)
-  return res.send(prettyBooks);
+  getBooks()
+  .then((booksData) => {
+    const prettyBooks = JSON.stringify(booksData, null, 4)
+    return res.send(prettyBooks);
+  })
+  .catch((error) => {
+    return res.status(500).send("No Books Retrieved.")
+  })
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = parseInt(req.params.isbn);
-  if (books[isbn]) {
-    return res.send("Enter Valid ISBN");
-  }
-  else {
-    return res.send(JSON.stringify(books[isbn], null, 4));
-  }
+
+  getBooks()
+  .then((booksData) => {
+    if (!booksData[isbn]) {
+        return res.send("Enter Valid ISBN");
+    } else {
+        return res.send(JSON.stringify(booksData[isbn], null, 4));
+    }
+  })
+  .catch((error) => {
+    return res.status(500).send("No Books Retrieved.")
+  })
+  
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
   
-  const booksArray = Object.values(books);
-  
-  const matchingBooks = booksArray.filter((book) => book.author === author);
+  getBooks()
+  .then((booksData) => {
+    const booksArray = Object.values(booksData);
+    const matchingBooks = booksArray.filter((book) => book.author === author);
 
-  if (matchingBooks.length > 0) {
-    return res.send(JSON.stringify(matchingBooks, null, 2)); // Nicely formatted
-  } else {
-    return res.status(404).send('No books found for this author');
-  }
-  //return res.status(300).json({message: "Yet to be implemented"});
+    if (matchingBooks.length > 0) {
+        return res.send(JSON.stringify(matchingBooks, null, 4)); // Nicely formatted
+    } else {
+        return res.status(404).send('No books found for this author');
+    }
+    })
+  .catch((error) => {
+    return res.status(500).send("No Books Retrieved.")
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
   
-    const booksArray = Object.values(books);
-    
+    getBooks()
+  .then((booksData) => {
+    const booksArray = Object.values(booksData);
     const matchingBooks = booksArray.filter((book) => book.title === title);
-  
+
     if (matchingBooks.length > 0) {
-      return res.send(JSON.stringify(matchingBooks, null, 2)); // Nicely formatted
+        return res.send(JSON.stringify(matchingBooks[0], null, 4)); // Nicely formatted
     } else {
-      return res.status(404).send('No books found with this title.');
+        return res.status(404).send('No books found by this Title.');
     }
+    })
+  .catch((error) => {
+    return res.status(500).send("No Books Retrieved.")
+  })
 });
 
 //  Get book review
